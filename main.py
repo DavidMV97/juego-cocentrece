3337f435bc4293884fb5e39c05234a5b61623b20
Germán David <gmunoz@seedem.co>
2024-03-13 21:37:43 -0500

    trabajando match botones

--- main.py
+++ main.py
@@ -8,9 +8,12 @@ class Aplicacion:
         self.window.geometry("500x400")
         self.window.resizable(False, False)
         
+    
+        self.count = 0
         self.valor_botones = [1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8]
         self.valor_boton = 0
         self.botones = []
+        self.buttons_couple= []
         
         for i in range(1, 17):
             self.valor_boton = self.valor_botones.pop(random.randrange(len(self.valor_botones))) 
@@ -29,7 +32,7 @@ class Aplicacion:
     
             self.boton1 = tk.Button(self.window, text=self.valor_boton, height=3, width=10)
             self.boton1.grid(column=self.colum_buttom, row=self.row_button)
-            self.boton1.config(command=lambda btn=self.boton1: self.click_button(btn))
+            self.boton1.config(command=lambda btn=self.boton1: self.show_button(btn))
             self.botones.append(self.boton1)
 
             
@@ -38,15 +41,61 @@ class Aplicacion:
         self.hide_buttons.grid(column=1, row=5)
         self.hide_buttons.config(command=self.ocultar_botones)
         self.window.mainloop()
-        
+    
+       
     def ocultar_botones(self):
         for boton in self.botones:
-            boton.config(background=boton.cget("background"), foreground=boton.cget("background"))
+            boton.config(background='orange', foreground='orange')
     
+    
+    #Validar botones activos
+    def show_button(self, button):
+        self.count += 1
+        button.config(background='blue', foreground='white', state='disabled')
+        value_button = button.cget('text')
         
-    def click_button(self, button):
-        print(button)
+
+        if self.count > 1:
+            for boton in self.botones:
+                if boton.cget('state') == 'disabled' and not boton.cget('background') == 'green':
+                    self.buttons_couple.append(boton)
+                    self.check_match_buttos(self.buttons_couple)
+                    
+            self.count = 0
     
+    
+    def check_match_buttos(self, buttons):
+        if len(buttons) > 1 :
+            value_first_button = buttons[0].cget('text')
+            value_second_button = buttons[1].cget('text')
+            
+            #Cooredenadas primer boton 
+            value_row_first_button = buttons[0].grid_info()['row']
+            value_column_first_button = buttons[0].grid_info()['column']
+            #Coordenadas segundo boton
+            value_row_second_button = buttons[1].grid_info()['row']
+            value_column_second_button = buttons[1].grid_info()['column']
+            # primer botón seleccionado
+            boton_select1 = tk.Button(self.window, height=3, width=10)
+            boton_select1.grid(column=value_column_first_button, row=value_row_first_button)
+            # segundo botón seleccionado
+            boton_select2 = tk.Button(self.window, height=3, width=10)
+            boton_select2.grid(column=value_column_second_button, row=value_row_second_button)
+            
+            if value_first_button == value_second_button:
+                boton_select1.config(background='green', state='disabled', text=value_first_button)
+                boton_select2.config(background='green', state='disabled', text=value_first_button)
+    
+            else:
+                boton_select1.config(background='orange', state='active', text=value_first_button)
+                boton_select2.config(background='orange', state='active', text=value_second_button)
+                #boton.config(background=boton.cget("background"), foreground=boton.cget("background"))
+
+            self.buttons_couple = []
+                
+
+        
+        #self.buttons_couple = []
 
         
         
