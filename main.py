e66f5a188009c70b6553e527af19bf4b3ec518dd
Germán David <gmunoz@seedem.co>
2024-03-16 15:50:21 -0500

    Corrigiendo funcion match buttons

--- main.py
+++ main.py
@@ -31,10 +31,10 @@ class Aplicacion:
                 self.row_button = 3
                 self.colum_buttom = i - 13
     
-            self.boton1 = tk.Button(self.window, text=self.valor_boton, height=3, width=10)
-            self.boton1.grid(column=self.colum_buttom, row=self.row_button)
-            self.boton1.config(command=lambda btn=self.boton1: self.show_button(btn))
-            self.botones.append(self.boton1)
+            self.boton = tk.Button(self.window, text=self.valor_boton, height=3, width=10)
+            self.boton.grid(column=self.colum_buttom, row=self.row_button)
+            self.boton.config(command=lambda btn=self.boton: self.validate_click_button(btn))
+            self.botones.append(self.boton)
 
             
         #Boton para ocultar los demas botones
@@ -49,79 +49,29 @@ class Aplicacion:
             boton.config(background='orange', foreground='orange')
     
     
-    #Validar botones activos
-    def show_button(self, button):
+    def validate_click_button(self, button):
         self.count += 1
         button.config(background='blue', foreground='white', state='disabled')
-        value_button = button.cget('text')
+        self.values_couple_buttons.append(button)
         
         if self.count > 1:
-            for boton in self.botones:
-                if boton.cget('state') == 'disabled' and not boton.cget('background') == 'orange':
-                    print('state buttons =>', boton.cget('state'), boton.cget('text'))
-                    self.buttons_couple.append(boton)
-                    print('len aqui =>' , len(self.buttons_couple))
-                    self.check_match_buttos(self.buttons_couple)
-                    
+            self.check_match_buttons(self.values_couple_buttons)      
             self.count = 0
-        
-    
-    def check_match_buttos(self, buttons):
-        
-        if len(self.values_couple_buttons) > 2 :
             self.values_couple_buttons = []
-        
-        for boton in buttons:
             
-            self.values_couple_buttons.append(boton.cget('text'))
             
-
-        print('couple buttons =>' , self.values_couple_buttons)
-        #if len(self.values_couple_buttons) > 2:
-        #
-        #self.values_couple_buttons = []
-        print('count =>', self.count)
+    
+    def check_match_buttons(self, buttons):
+        value_first_button = buttons[0].cget('text')
+        value_second_button = buttons[1].cget('text')
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        #value_first_button = buttons[0].cget('text')
-        #value_second_button = buttons[1].cget('text')
-            
-            #Cooredenadas primer boton 
-            # value_row_first_button = buttons[0].grid_info()['row']
-            # value_column_first_button = buttons[0].grid_info()['column']
-            # #Coordenadas segundo boton
-            # value_row_second_button = buttons[1].grid_info()['row']
-            # value_column_second_button = buttons[1].grid_info()['column']
-            # # primer botón seleccionado
-            # boton_select1 = tk.Button(self.window, height=3, width=10)
-            # boton_select1.grid(column=value_column_first_button, row=value_row_first_button)
-            # # segundo botón seleccionado
-            # boton_select2 = tk.Button(self.window, height=3, width=10)
-            # boton_select2.grid(column=value_column_second_button, row=value_row_second_button)
-            
-        #print('value first button =>',  value_first_button)
-        #print('value second button =>',  value_second_button)
-            
-        # if value_first_button == value_second_button:
-        #     print('Match de botones')
-            #boton_select1.config(background='green', state='disabled', text=value_first_button)
-            #boton_select2.config(background='green', state='disabled', text=value_first_button)
-                
-        # else:
-        #     print('no match')
-            #boton_select1.config(background='orange', state='normal', text=value_first_button)
-            #boton_select2.config(background='orange', state='normal', text=value_second_button)
-            #boton.config(background=boton.cget("background"), foreground=boton.cget("background"))
-                
-            
+        if value_first_button == value_second_button:
+            buttons[0].config(background='green', foreground='white', state='disabled')
+            buttons[1].config(background='green', foreground='white', state='disabled')
+        else:
+            buttons[0].config(background='orange', foreground='orange', state='normal')
+            buttons[1].config(background='orange', foreground='orange', state='normal')
+                    
         self.buttons_couple = []
 
         
