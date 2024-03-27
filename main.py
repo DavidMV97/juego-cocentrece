840624c452da88225a441d737ca18d8ee0f39ddc
Germán David <gmunoz@seedem.co>
2024-03-26 22:39:21 -0500

    creando timer

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from tkinter import ttk
 from tkinter import messagebox
 import random 
 import pygame
+import time
+
 
 pygame.mixer.init()
 
@@ -18,32 +20,39 @@ class Aplicacion:
         self.window.config(background='#1f1e30')
         # errores a la hora de hacer match
         self.num_errores = 0
-        self.label_errores = tk.Label(self.window, text=f'Errores : {self.num_errores}', font=("Helvetica", 17), width=9, padx=5)
+        self.label_errores = tk.Label(self.window, text=f'Errores : {self.num_errores}', font=("Helvetica", 19), width=9, padx=5)
         self.label_errores.grid(row=0,column=0, pady=5)
         self.label_errores.config(background='white')
         #contar clicks
         self.count = 0
         #contar coincidencias
         self.counter_match = 0
+        
         self.standar_background = '#575769'
+        
         self.valor_boton = 0
+        
         self.botones = []
+        
         self.values_couple_buttons = []
         #Marco inferior
         self.botton_frame = ttk.Frame(self.window)
         self.botton_frame.grid(row=5, column=0, columnspan=4, pady=12)
         
+        self.time_label = tk.Label(self.window, text="00:00",background='white', font=("Helvetica", 16))
+        self.time_label.grid(row=0, column=1)
+                
         self.create_buttons()
-            
-        self.hide_buttons = tk.Button(self.botton_frame, text='Ocultar botones', borderwidth=0, relief='flat')
-        self.hide_buttons.config(command=self.ocultar_botones, 
+        # Botón ocultar todos los botones  
+        self.hide = tk.Button(self.botton_frame, text='Ocultar botones', borderwidth=0, relief='flat')
+        self.hide.config(command=self.hide_buttons, 
                                  width=12,
                                  background='#ff9703', foreground='white',
                                  activebackground='#ff9703',
                                  activeforeground='white',
                                  pady=5,
                                  cursor="hand1")
-        self.hide_buttons.pack(side="bottom")
+        self.hide.pack(side="bottom")
         self.window.mainloop()
     
     
@@ -82,10 +91,12 @@ class Aplicacion:
      
     
        
-    def ocultar_botones(self):
+    def hide_buttons(self):
         for boton in self.botones:
             boton.config(background=self.standar_background, foreground=self.standar_background, activebackground=self.standar_background, activeforeground=self.standar_background, state='normal')
-        self.hide_buttons.config(state='disabled')
+        self.hide.config(state='disabled')
+         # Inicio el temporizador
+        self.start_timer()
     
     
     def validate_click_button(self, button):
@@ -116,7 +127,8 @@ class Aplicacion:
             buttons[1].config(background=self.standar_background, foreground=self.standar_background, state='normal')
             self.num_errores += 1
             self.update_label_errors() 
-                    
+        
+        # Valido fin del juego             
         if self.counter_match == 8:
             self.num_errores = 0
             nuew_game = messagebox.askyesno("Juego terminado", "¿Quieres jugar de nuevo?")
@@ -129,13 +141,37 @@ class Aplicacion:
     def new_game(self):
         self.create_buttons()
         self.counter_match = 0
-        self.hide_buttons.config(state='normal')
+        self.hide.config(state='normal')
         self.label_errores.config(text=f'Errores : {0}') 
+        self.time_label.config(text='00:00')
         
+         
         
-    
     def update_label_errors(self):
         self.label_errores.config(text=f'Errores : {self.num_errores}') 
+        
+    
+    def start_timer(self):
+        self.initial_time = time.time()  
+        self.update_timer() 
+    
+    
+    def update_timer(self):
+        # Calculo el tiempo transcurrido
+        time_elapsed = int(time.time() - self.initial_time) 
+        format_time = self.time_format(time_elapsed)  # Formatear el tiempo
+        # Actualizo la etiqueta
+        self.time_label.config(text=f"{format_time}")  
+        
+        if self.counter_match != 8:
+            self.window.after(1000, self.update_timer)
+    
+    
+    def time_format(self, seconds):
+        minutes = seconds // 60
+        seconds_remaining = seconds % 60
+        return f"{minutes:02}:{seconds_remaining:02}"
+
     
     
     
